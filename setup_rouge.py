d5904e471bc76aeca5148d8bf2b6d12ee3f89f0a
Hiroaki_Hayashi <hiroakihayash88@gmail.com>
2020-07-31 15:15:19 -0400

    Make path selection the default

--- setup_rouge.py
+++ setup_rouge.py
@@ -17,12 +17,7 @@ def copy_rouge():
     src_rouge_root = "./files2rouge/RELEASE-1.5.5/"
 
     default_root = os.path.join(home, '.files2rouge/')
-
-    print("files2rouge uses scripts and tools that will not be stored with "
-          "the python package")
-    path = input(
-        "where do you want to save it? [default: %s]" % default_root)
-
+    path = ""
     if path == "":
         path = default_root
 
